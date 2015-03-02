e3e474f15ef7813984f38568edfc894476c684bb
Brendan Johnson <bjohnson@paragusit.com>
2015-03-02 16:34:37 -0500

    added comment to client.go

--- backends/client.go
+++ backends/client.go
@@ -1,5 +1,7 @@
 package backends
 
+// The StoreClient interface is implemented by objects that can retrieve
+// key/value pairs from a backend store.
 type StoreClient interface {
 	GetValues(key []string) (map[string]string, error)
 	WatchPrefix(prefix string, waitIndex uint64, stopChan chan bool) (uint64, error)
